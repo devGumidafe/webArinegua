27d9f04f1958bf4a85e774a6f7eab62bd5491acd
Gumidafe Socorro <devgumidafe@gmail.com>
2022-11-10 17:32:24 +0000

    add logo image

--- src/components/UI/logo.jsx
+++ src/components/UI/logo.jsx
@@ -1,11 +1,12 @@
-import { chakra } from "@chakra-ui/react";
-import NextLink from "next/link";
+import { chakra } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import Image from 'next/image';
 
 const Logo = () => {
   return (
-    <NextLink href="/" passHref>
-      <chakra.a fontSize="2rem" fontWeight="700">
-        LOGO
+    <NextLink href='/' passHref>
+      <chakra.a fontSize='2rem' fontWeight='700'>
+        <Image src='/images/logo.png' alt='Logo' width={70} height={70} />
       </chakra.a>
     </NextLink>
   );
